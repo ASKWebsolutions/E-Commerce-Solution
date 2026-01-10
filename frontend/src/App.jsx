3cd573a3296926a07d3e2ab452dc846f69ab0dae
coder7190 <shahzaibnawaz719@gmail.com>
2026-01-10 18:48:45 +0500

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import Login from './Login'
+import Products from './Products'
+import Register from './Register'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+<Register />
+<hr />
+    <Login />
+    <hr />
+    <Products />
+    </>
+  )
+}
+
+export default App
